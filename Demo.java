637ef46b64c2005de743852fd75219a7f3bb1e5a
abc@gmail.com <venkat@gmail.com>
2024-12-22 23:21:11 +0530

    Added java file

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo{
+     public static void main (String [] args){
+     System.out.println ("Hello from Demo");
+           System.out.println ("Hello to sweta from demo java");
+               System.out.println ("Hello to all from demo java");
+               System.out.println ("Hello everyone from demo java");
+       }
+}
