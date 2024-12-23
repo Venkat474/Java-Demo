1e2be13bbedf7454a61cba16dff373997fe326c3
Venkat474 <vappaji07@gmail.com>
2024-12-23 23:38:03 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Demo{
      public static void main (String [] args){
      System.out.println ("Hello from Demo");
-           System.out.println ("Hello to sweta from demo java");
+           System.out.println ("Hello to amol from demo java");
                System.out.println ("Hello to all from demo java");
                System.out.println ("Hello everyone from demo java");
        }
